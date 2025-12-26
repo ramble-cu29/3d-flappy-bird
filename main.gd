47bf747b1f61cc61af9ace6f8d31138c20e0a515
Ramble <rambleinthebramble@proton.me>
2025-12-25 16:07:57 -0900

    Moved initialize() function into main.gd and generalized wall creation further.

--- main.gd
+++ main.gd
@@ -2,37 +2,42 @@ extends Node
 
 signal despawn
 
-@export var wall_scene: PackedScene
+@export var speed = -5
+var wall = preload("res://wall.tscn")
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$UserInterface/Message.hide()
 	
-
- 
-
 func _on_player_ded():
 	$UserInterface/Message.show()
 
+func initialize(wall_obj: CharacterBody3D, height: float, coord: float):
+	var mesh = wall_obj.get_node("MeshInstance3D")
+	var col_shape = wall_obj.get_node("Area3D/CollisionShape3D")
+	var size = Vector3(1.0, height, 20.0)
+	var pos = Vector3(50.0, coord, 0.0)
 
+	wall_obj.velocity = Vector3(speed, 0, 0)
+	wall_obj.global_position = pos
+	
+	mesh.mesh.size = size
+	col_shape.shape.size = size
+	
 func _on_wall_timer_timeout() -> void:
-	
-	
-	
 	# Add upper and lower walls to scene tree
-	var upper = load("res://wall.tscn").instantiate()
-	var lower = load("res://wall.tscn").instantiate()
-	
-	# Initialize function, basically give it a velocity and run move_and_slide()
-	var size = Vector3(1.0, 1.5, 20.0)
-	var position_upper = Vector3(50.0, 8.0, 0.0)
-	var position_lower = Vector3(50.0, 4.0, 0.0)
-	
-	upper.initialize(size, position_upper)
-	lower.initialize(size, position_lower)
-	
+	var upper = wall.instantiate()
 	add_child(upper)
-	add_child(lower)
+	var height_u = 1.0
+	var coord_u = 12.0
+	initialize(upper, height_u, coord_u)
 
+	var lower = wall.instantiate()
+	add_child(lower)
+	var height_l = 4.0 
+	var coord_l = 4.0
+	initialize(lower, height_l, coord_l)
+		
 func _on_despawner_body_entered() -> void:
 	despawn.emit()
--- wall.gd
+++ wall.gd
@@ -4,13 +4,3 @@ extends CharacterBody3D
 
 func _physics_process(delta):
 	move_and_slide()
-
-func teleport(point: Vector3) -> void:
-	global_position = point
-
-func initialize(size: Vector3, position: Vector3):
-	velocity = Vector3(speed,0,0)
-	$Area3D/CollisionShape3D.shape.size = size
-	$MeshInstance3D.mesh.size = size 
-	teleport(position)
-	
